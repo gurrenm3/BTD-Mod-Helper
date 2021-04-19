e7f711c1f26beedc75c4982f1cbfc4ffa77c2040
doombubbles <obsidiantoolzg@gmail.com>
2021-04-18 20:18:22 -0700

    Prototype expanded mod updater

--- BloonsTD6 Mod Helper/MelonMain.cs
+++ BloonsTD6 Mod Helper/MelonMain.cs
@@ -4,11 +4,14 @@ using BTD_Mod_Helper.Api;
 using BTD_Mod_Helper.Api.InGame_Mod_Options;
 using MelonLoader;
 using System;
+using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 using Assets.Scripts.Unity.UI_New.Popups;
 using BTD_Mod_Helper.Api.Updater;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace BTD_Mod_Helper
 {
@@ -17,14 +20,14 @@ namespace BTD_Mod_Helper
         internal static string modDir = $"{Environment.CurrentDirectory}\\Mods\\{Assembly.GetExecutingAssembly().GetName().Name}";
 
         public const string githubReleaseURL = "https://api.github.com/repos/gurrenm3/BTD-Mod-Helper/releases";
+        public const string githubLatestURL = "https://github.com/gurrenm3/BTD-Mod-Helper/releases/latest";
         public const string coopMessageCode = "BTD6_ModHelper";
         public const string currentVersion = "1.0.0";
 
         private bool useModOptionsDEBUG = false;
         private ModOptionsMenu modOptionsUI;
 
-        bool isUpdateAvailable = false;
-        bool finishedCheckingForUpdates = false;
+        private Dictionary<string, string> updateURLs = new Dictionary<string, string>();
 
 
         public override async void OnApplicationStart()
@@ -34,8 +37,35 @@ namespace BTD_Mod_Helper
             MelonLogger.Msg("Checking for updates...");
             var updater = new UpdateChecker(githubReleaseURL);
             var releaseInfo = await updater.GetLatestReleaseAsync();
-            isUpdateAvailable = updater.IsUpdate(Info.Version, releaseInfo);
-            finishedCheckingForUpdates = true;
+            if (updater.IsUpdate(Info.Version, releaseInfo))
+            {
+                updateURLs[Info.Name] = githubLatestURL;
+            }
+
+            Parallel.ForEach(
+                MelonHandler.Mods.OfType<BloonsTD6Mod>(),
+                async mod =>
+                {
+                    if (mod.GithubReleaseURL != "")
+                    {
+                        try
+                        {
+                            updater = new UpdateChecker(mod.GithubReleaseURL);
+                            releaseInfo = await updater.GetLatestReleaseAsync();
+                            if (updater.IsUpdate(mod.Info.Version, releaseInfo))
+                            {
+                                updateURLs[mod.Info.Name] = mod.LatestURL;
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            MelonLogger.Warning($"Encountered exception trying to check {mod.Info.Name} for updates: ");
+                            MelonLogger.Warning(e.Message);
+                        }
+                    }
+                }
+            );
+            
             MelonLogger.Msg("Finished checking for updates.");
         }
 
@@ -44,18 +74,18 @@ namespace BTD_Mod_Helper
             if (Game.instance is null)
                 return;
 
-            if (finishedCheckingForUpdates && isUpdateAvailable && PopupScreen.instance != null)
+            if (PopupScreen.instance != null && updateURLs.Count > 0)
             {
-                string latestVersionUrl = "https://github.com/gurrenm3/BTD-Mod-Helper/releases/latest";
-                MelonLogger.Msg("An update is available for this mod.");
-                PopupScreen.instance.ShowPopup(PopupScreen.Placement.menuCenter, "An Update is Available!",
-                    $"An update is available for the mod \"{Info.Name}\". Would you like to be taken to the download page?\n\n" +
-                    $"NOTE: Some mods may not work if you aren't using the latest version of this mod",
-                    new Action(() => { Process.Start(latestVersionUrl); }), "YES", new Action(() => { }),
-                    "Not now", Popup.TransitionAnim.Update, instantClose:true);
-
-                finishedCheckingForUpdates = false;
-                isUpdateAvailable = false;
+                foreach (var (modName, updateURL) in updateURLs)
+                {
+                    PopupScreen.instance.ShowPopup(PopupScreen.Placement.menuCenter, "An Update is Available!",
+                        $"An update is available for the mod \"{modName}\". Would you like to be taken to the download page?\n\n" +
+                        $"NOTE: Some mods may not work if you aren't using the latest version of this mod",
+                        new Action(() => { Process.Start(updateURL); }), "YES", new Action(() => { }),
+                        "Not now", Popup.TransitionAnim.Update, instantClose:true);
+                }
+                
+                updateURLs.Clear();
             }
 
             // used to test new api methods
@@ -104,12 +134,9 @@ namespace BTD_Mod_Helper
 
         public static void DoPatchMethods(Action<BloonsTD6Mod> action)
         {
-            foreach (var melonMod in MelonHandler.Mods)
+            foreach (var mod in MelonHandler.Mods.OfType<BloonsTD6Mod>())
             {
-                if (melonMod is BloonsTD6Mod mod)
-                {
-                    action.Invoke(mod);
-                }
+                action.Invoke(mod);
             }
         }
         
