1e217e15e44f685750f49dea578b62048e80d433
Thomas <thomaspmar@protonmail.com>
2021-05-04 10:43:55 -0700

    Added System.Type.GetMethods("methodName") and Array.FindAll

---
+++ BTD Mod Helper Core/Extensions/LINQExtensions/ArrayExt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BTD_Mod_Helper.Extensions
+{
+    public static partial class ArrayExt
+    {
+        /// <summary>
+        /// Retrieves all the elements that match the conditions defined by the specified predicate.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <param name="match"> The Predicate<T> delegate that defines the conditions of the elements to search for.</param>
+        /// <returns></returns>
+        public static T[] FindAll<T>(this T[] array, Predicate<T> match)
+        {
+            return Array.FindAll(array, match);
+        }
+    }
+}
---
+++ BTD Mod Helper Core/Extensions/SystemExtensions/TypeExt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace BTD_Mod_Helper.Extensions
+{
+    public static class TypeExt
+    {
+        public static MethodInfo[] GetMethods(this Type type, string name)
+        {
+            return type.GetMethods().FindAll(method => method.Name == name);
+        }
+    }
+}
