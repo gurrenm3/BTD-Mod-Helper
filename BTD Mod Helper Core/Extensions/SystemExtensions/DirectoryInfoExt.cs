4b860a100fd83d66e44a42306ce630d5f7307023
Thomas <thomaspmar@protonmail.com>
2021-05-14 20:58:42 -0700

    Added extensions to getting all melonloader mods through reflection

---
+++ BTD Mod Helper Core/Extensions/SystemExtensions/DirectoryInfoExt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace BTD_Mod_Helper.Extensions
+{
+    public static class DirectoryInfoExt
+    {
+        /// <summary>
+        /// (Cross-Game compatible) Returns all Files in this directory that reference MelonLoader.dll or MelonLoader.ModHandler.dll
+        /// </summary>
+        /// <param name="directoryInfo"></param>
+        /// <returns></returns>
+        public static FileInfo[] GetAllMelonMods(this DirectoryInfo directoryInfo)
+        {
+            var files = directoryInfo.GetFiles();
+            if (!files.Any())
+                return null;
+
+            return Array.FindAll(files, file => file.IsMelonMod());
+        }
+    }
+}
