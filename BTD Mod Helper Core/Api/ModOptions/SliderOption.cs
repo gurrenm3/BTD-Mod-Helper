3a066e22c9c5167c6f5266602775e734876a9c99
Thomas <thomaspmar@protonmail.com>
2021-06-24 14:58:29 -0700

    Resized Toggle button text and added Text element for the SliderOption

--- BTD Mod Helper Core/Api/ModOptions/SliderOption.cs
+++ BTD Mod Helper Core/Api/ModOptions/SliderOption.cs
@@ -9,35 +9,42 @@ namespace BTD_Mod_Helper.Api.ModOptions
         public Slider slider;
         public Image fill;
         public Image handle;
+        public Text sliderValueText;
 
         private SliderOption(GameObject parentGO, ModSetting modSetting) :  base(parentGO, modSetting, "SliderOption")
         {
-            MelonLoader.MelonLogger.Msg("---");
-            MelonLoader.MelonLogger.Msg("SliderOption Info:");
-            MelonLoader.MelonLogger.Msg(rectTransform.position.x);
-            MelonLoader.MelonLogger.Msg(rectTransform.position.y);
-
             slider = instantiatedGameObject.transform.Find("Slider").GetComponent<Slider>();
             fill = instantiatedGameObject.transform.Find("Slider/Fill Area/Fill").GetComponent<Image>();
             handle = instantiatedGameObject.transform.Find("Slider/Handle Slide Area/Handle").GetComponent<Image>();
+            sliderValueText = instantiatedGameObject.transform.Find("Slider/SliderValue").GetComponent<Text>();
         }
 
         public SliderOption(GameObject parentGO, ModSettingInt modSettingInt) : this(parentGO, (ModSetting)modSettingInt)
         {
             slider.value = modSettingInt.value;
+            sliderValueText.text = modSettingInt.value.ToString();
             slider.minValue = (long) modSettingInt.minValue;
             slider.maxValue = (long) modSettingInt.maxValue;
             
-            slider.onValueChanged.AddListener(value => modSettingInt.SetValue((long)value));
+            slider.onValueChanged.AddListener(value => 
+            {
+                modSettingInt.SetValue((long)value);
+                sliderValueText.text = value.ToString();
+            });
         }
         
         public SliderOption(GameObject parentGO, ModSettingDouble modSettingDouble) : this(parentGO, (ModSetting)modSettingDouble)
         {
             slider.value = (float) modSettingDouble.value;
+            sliderValueText.text = modSettingDouble.value.ToString("C2");
             slider.minValue = (float) modSettingDouble.minValue;
             slider.maxValue = (float) modSettingDouble.maxValue;
             
-            slider.onValueChanged.AddListener(value => modSettingDouble.SetValue((double)value));
+            slider.onValueChanged.AddListener(value => 
+            {
+                modSettingDouble.SetValue((double)value);
+                sliderValueText.text = value.ToString("C2");
+            });
         }
 
         internal static RectTransform GetOriginalAsset(GameObject parentGO)
