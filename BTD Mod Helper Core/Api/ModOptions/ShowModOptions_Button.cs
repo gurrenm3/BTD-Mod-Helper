20fac0d37adee91eeadc24a495f3a9b61fb1bb2c
Silentstorm <29950414+Onixiya@users.noreply.github.com>
2021-10-16 03:30:55 +1100

    fix for btd6 mod options button position

--- BTD Mod Helper Core/Api/ModOptions/ShowModOptions_Button.cs
+++ BTD Mod Helper Core/Api/ModOptions/ShowModOptions_Button.cs
@@ -8,6 +8,7 @@ using BTD_Mod_Helper.Extensions;
 using BTD_Mod_Helper.Patches;
 using MelonLoader;
 using Object = UnityEngine.Object;
+using UnityEngine.UI.Extensions;
 
 namespace BTD_Mod_Helper.Api.ModOptions
 {
@@ -26,25 +27,29 @@ namespace BTD_Mod_Helper.Api.ModOptions
 
         public void Init()
         {
-#if BloonsTD6
+            #if BloonsTD6
             var scene = SceneManager.GetSceneByName("SettingsUI");
-#elif BloonsAT
+            #elif BloonsAT
             var scene = SceneManager.GetSceneByName("UI-Settings");
-#endif
+            #endif
             var rootGameObjects = scene.GetRootGameObjects();
             settingsUI_Canvas = rootGameObjects[0];
             optionsButton = ModOptionsMenu.CanvasGO.transform.Find("ModOptionsButton/Button").GetComponent<Button>();
 
             var twitchButton = settingsUI_Canvas.GetComponentInChildrenByName<RectTransform>("TwitchButton");
             
-            
+            twitchButton.name="ModOptionsButton";
             instantiatedButton = Object.Instantiate(optionsButton, twitchButton.transform);
             instantiatedButton.onClick.AddListener(OptionButtonClicked);
 
             var transform = instantiatedButton.transform.Cast<RectTransform>();
-            
+
             transform.Translate(new Vector3(-150, 0), twitchButton.transform);
             transform.localScale = new Vector3(2.5f, 2.5f);
+            #if BloonsTD6
+            transform.position=new Vector3(1520,-5,0);
+            settingsUI_Canvas.GetComponentInChildrenByName<NonDrawingGraphic>("ClickBlock").gameObject.Destroy();
+            #endif
         }
 
         public void OptionButtonClicked()
