5186c5610d4ceaecdbc05dd4095abe5ce34100fb
Thomas Margraff <thomaspmar@protonmail.com>
2021-11-11 14:03:46 -0800

    Adding RegisterInIl2Cpp attribute

---
+++ BTD Mod Helper Core/Api/Custom Monobehaviors/CustomComponentTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace BTD_Mod_Helper.Api
+{
+    internal class CustomComponentTest : MonoBehaviour
+    {
+        void Update()
+        {
+            MelonLoader.MelonLogger.Msg("Custom component test");
+        }
+    }
+}
