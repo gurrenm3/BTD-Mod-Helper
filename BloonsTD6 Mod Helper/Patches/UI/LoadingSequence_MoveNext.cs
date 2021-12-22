19497a226c2d3be1d6d99f8b946b11a0f96b2e63
doombubbles <obsidiantoolzg@gmail.com>
2021-12-21 22:34:05 -0800

    Begin LoadingSequence additions

---
+++ BloonsTD6 Mod Helper/Patches/UI/LoadingSequence_MoveNext.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using MelonLoader;
+using Main = Assets.Main.Main;
+
+namespace BTD_Mod_Helper.Patches.UI
+{
+    [HarmonyPatch(typeof(Main._LoadingSequence_d__42), nameof(Main._LoadingSequence_d__42.MoveNext))]
+    internal static class LoadingSequence_MoveNext
+    {
+        [HarmonyPrefix]
+        private static bool Prefix(Main._LoadingSequence_d__42 __instance)
+        {
+            return true;
+        }
+
+        [HarmonyPostfix]
+        private static void Postfix(Main._LoadingSequence_d__42 __instance)
+        {
+            MelonLogger.Msg($"moving next! {__instance.__1__state}");
+            
+        }
+    }
+}
---
+++ BloonsTD6 Mod Helper/Patches/UI/Main_UpdateStatusMessage.cs
@@ -0,0 +1,23 @@
+﻿using Assets.Main;
+using Assets.Scripts.Unity.Tasks;
+using HarmonyLib;
+
+namespace BTD_Mod_Helper.Patches.UI
+{
+    [HarmonyPatch(typeof(Main), nameof(Main.UpdateStatusMessage))]
+    internal static class Main_UpdateStatusMessage
+    {
+        [HarmonyPrefix]
+        private static bool Prefix(Main __instance, SeriesTasks tasks)
+        {
+            
+            return true;
+        }
+
+        [HarmonyPostfix]
+        private static void Postfix(Main __instance)
+        {
+
+        }
+    }
+}
