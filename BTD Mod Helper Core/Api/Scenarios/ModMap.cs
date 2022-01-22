7ab7c562d0e3352c3c618ac7f734e5cc8ece7122
Thomas Margraff <thomaspmar@protonmail.com>
2022-01-22 09:32:26 -0800

    Added a bit to ModMap

--- BTD Mod Helper Core/Api/Scenarios/ModMap.cs
+++ BTD Mod Helper Core/Api/Scenarios/ModMap.cs
@@ -1,17 +1,47 @@
-﻿using MelonLoader;
+﻿using Assets.Scripts.Data.MapSets;
+using Assets.Scripts.Models.Map;
+using MelonLoader;
 
 namespace BTD_Mod_Helper.Api
 {
     /// <summary>
-    /// 
+    /// Class for creating custom Maps easier. 
     /// </summary>
     public abstract class ModMap : ModContent
     {
+        /// <summary>
+        /// The name of this map.
+        /// </summary>
+        public abstract new string Name { get; }
+
+        /// <summary>
+        /// The difficulty of this map.
+        /// </summary>
+        public abstract MapDifficulty Difficulty { get; }
+
+        /// <summary>
+        /// The <see cref="MapModel"/> associated with this modded map.
+        /// </summary>
+        public MapModel Map { get => (map == null) ? map = CreateMapModel() : map; }
+        private MapModel map;
+
+
+        /// <summary>
+        /// Creates an instance of this <see cref="ModMap"/>.
+        /// </summary>
         public ModMap()
         {
-            //var mapModel = new MapModel("", null, null, null, null, null, null, 3, null, null, 4); // testing if it's possible to create map model
-            
+            // Line below is for reference to see what parameters are needed for a MapModel
+            //var mapModel = new MapModel("", null, null, null, null, null, null, 3, null, null, 4); 
         }
+
+        /// <summary>
+        /// Used this to create the actual MapModel for this map. It will automatically
+        /// be called the first time <see cref="Map"/> is accessed and the result will be
+        /// cached.
+        /// </summary>
+        /// <returns></returns>
+        protected abstract MapModel CreateMapModel();
 
         /// <summary>
         /// <inheritdoc/>
