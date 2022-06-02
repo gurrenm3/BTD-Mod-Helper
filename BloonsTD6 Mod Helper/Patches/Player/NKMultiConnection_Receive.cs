8987adc185df362866552922498fd308a2d33ea3
Sewer 56 <seweratwork@gmail.com>
2022-06-02 23:03:06 +0100

    Bugfix: Handle all custom messages in NKMultiConnection.Receive

--- BloonsTD6 Mod Helper/Patches/Player/NKMultiConnection_Receive.cs
+++ BloonsTD6 Mod Helper/Patches/Player/NKMultiConnection_Receive.cs
@@ -12,8 +12,9 @@ namespace BTD_Mod_Helper.Patches
             var messageQueue = __instance.ReceiveQueue;
             if (messageQueue == null || messageQueue.Count == 0)
                 return;
-            
-            for (int i = 0; i < messageQueue.Count; i++)
+
+            var messageCount = messageQueue.Count;
+            for (int i = 0; i < messageCount; i++)
             {
                 var message = messageQueue.Dequeue();
                 bool consumed = false;
@@ -28,5 +29,4 @@ namespace BTD_Mod_Helper.Patches
             }
         }
     }
-
 }
