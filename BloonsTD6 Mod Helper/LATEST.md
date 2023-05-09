08590475c3df83dd40ae7e0cdce381416f5e60fa
doombubbles <obsidiantoolzg@gmail.com>
2023-05-08 18:59:58 -0700

    Add ModSubTower

--- BloonsTD6 Mod Helper/LATEST.md
+++ BloonsTD6 Mod Helper/LATEST.md
@@ -3,4 +3,5 @@
 - `FileIOHelper.SaveObject` now will use `ReferenceLoopHandling.Ignore` by default
     - (More Alchemist TowerModels will now be included in the exported game data)
 - Added `ModTower.ShopTowerCount` override to easily set how many of a tower you can purchase at once in a standard game
+- Added a `ModSubTower` class that streamlines the steps needed to make a subtower for things like `TowerCreateTowerModel`
 - Some fixes for ModBloon default displays
--- BloonsTD6 Mod Helper/Patches/Towers/TowerInventory_Init.cs
+++ BloonsTD6 Mod Helper/Patches/Towers/TowerInventory_Init.cs
@@ -1,23 +1,48 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using BTD_Mod_Helper.Api;
+using BTD_Mod_Helper.Api.Towers;
 using Il2CppAssets.Scripts.Models.TowerSets;
 using Il2CppAssets.Scripts.Simulation.Input;
+using Il2CppAssets.Scripts.Unity;
 namespace BTD_Mod_Helper.Patches.Towers;
 
 [HarmonyPatch(typeof(TowerInventory), nameof(TowerInventory.Init))]
 internal static class TowerInventory_Init
 {
     [HarmonyPrefix]
-    private static bool Prefix(ref TowerInventory __instance, ref IEnumerable<TowerDetailsModel> allTowersInTheGame)
+    private static bool Prefix(ref TowerInventory __instance,
+        ref Il2CppSystem.Collections.Generic.IEnumerable<TowerDetailsModel> allTowersInTheGame)
     {
         var result = true;
         var unref__instance = __instance;
-        var unref_allTowersInTheGame = allTowersInTheGame;
-        ModHelper.PerformAdvancedModHook(mod => result &=  mod.PreTowerInventoryInit(ref unref__instance, ref unref_allTowersInTheGame));
+        var list = allTowersInTheGame.ToList();
+        var unref_allTowersInTheGame = (IEnumerable<TowerDetailsModel>) list;
+
+        ModHelper.PerformAdvancedModHook(mod =>
+            result &= mod.PreTowerInventoryInit(ref unref__instance, ref unref_allTowersInTheGame));
         __instance = unref__instance;
-        allTowersInTheGame = unref_allTowersInTheGame;
+
+        var allTowers = Game.instance.model.towerSet.ToList();
+
+        foreach (var modTower in ModContent.GetContent<ModTower>())
+        {
+            var towerModel = Game.instance.model.GetTowerWithName(modTower.Id);
+
+            if (modTower.DontAddToShop && !towerModel.isSubTower)
+            {
+                var index = Math.Clamp(modTower.GetTowerIndex(allTowers), 0, allTowers.Count);
+                list.Insert(index, new ShopTowerDetailsModel(modTower.Id, index, 5, 5, 5, modTower.ShopTowerCount, 0));
+            }
+        }
+
+        allTowersInTheGame = list.ToIl2CppReferenceArray()
+            .Cast<Il2CppSystem.Collections.Generic.IEnumerable<TowerDetailsModel>>();
+
         return result;
     }
+
     [HarmonyPostfix]
     private static void Postfix(TowerInventory __instance, IEnumerable<TowerDetailsModel> allTowersInTheGame)
     {
