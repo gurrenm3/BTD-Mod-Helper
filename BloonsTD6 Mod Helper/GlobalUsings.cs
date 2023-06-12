ff857d1a26787e66eab756b86c4f221cff677737
doombubbles <obsidiantoolzg@gmail.com>
2023-06-12 11:14:45 -0700

    Fix IsExternalInit

--- BloonsTD6 Mod Helper/GlobalUsings.cs
+++ BloonsTD6 Mod Helper/GlobalUsings.cs
@@ -5,3 +5,17 @@ global using Il2CppInterop.Runtime.InteropTypes;
 global using Il2CppInterop.Runtime.Runtime;
 global using Il2CppInterop.Runtime.InteropTypes.Arrays;
 global using Il2Cpp;
+using System.ComponentModel;
+
+// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// Reserved to be used by the compiler for tracking metadata.
+    /// This class should not be used by developers in source code.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    internal static class IsExternalInit
+    {
+    }
+}
