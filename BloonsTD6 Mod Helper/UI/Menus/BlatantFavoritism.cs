c022e58b1bbad5f5de5d80781f6f4fcfd241637c
Edward Frye <46543066+AnakinSkywalker066@users.noreply.github.com>
2023-07-12 23:49:37 -0400

    Update BlatantFavoritism.cs

--- BloonsTD6 Mod Helper/UI/Menus/BlatantFavoritism.cs
+++ BloonsTD6 Mod Helper/UI/Menus/BlatantFavoritism.cs
@@ -10,7 +10,7 @@ internal class BlatantFavoritism
             "doombubbles" => new Color32(200, 0, 255, 255),
             "Btd6ModHelper" => new Color32(200, 75, 255, 255),
             "gurrenm3" => new Color32(200, 150, 255, 255),
-            "Anakinskywalker066 & Jonylovespie" => new Color32(255,200,200,255),
+            "Jonylovespie" => new Color32(255,200,200,255),
             "Anakinskywalker066" => new Color32(200, 0, 255, 255),
             _ => Color.white
         };
