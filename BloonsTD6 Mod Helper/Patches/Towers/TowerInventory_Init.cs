a8b977b4b4b549eb6ca2d296ad36c02d9c6d42f7
silentstorm <what@what.com>
2023-08-08 17:37:41 +1000

    Fix for crashing subtowers on towerinventory

--- BloonsTD6 Mod Helper/Patches/Towers/TowerInventory_Init.cs
+++ BloonsTD6 Mod Helper/Patches/Towers/TowerInventory_Init.cs
@@ -32,16 +32,28 @@ internal static class TowerInventory_Init
 /// <summary>
 /// Prevent crashes for non-subtowers that aren't in the inventory
 /// </summary>
-[HarmonyPatch]
 internal static class TowerInventory_Patches
 {
-    private static System.Collections.Generic.IEnumerable<MethodBase> TargetMethods()
-    {
-        yield return AccessTools.Method(typeof(TowerInventory), nameof(TowerInventory.UpdatedTower));
-        yield return AccessTools.Method(typeof(TowerInventory), nameof(TowerInventory.DestroyedTower));
-        yield return AccessTools.Method(typeof(TowerInventory), nameof(TowerInventory.CreatedTower));
+    [HarmonyPatch(typeof(TowerInventory),"CreatedTower")]
+    public class TowerInventoryCreatedTower_Patch{
+        [HarmonyPrefix]
+        public static bool Prefix(ref TowerInventory __instance,ref TowerModel def){
+            if(!__instance.towerCounts.ContainsKey(def.baseId)){
+                __instance.towerCounts.Add(def.baseId,0);
+            }
+	        __instance.towerCounts[def.baseId]=__instance.towerCounts[def.baseId]+1;
+	        return false;
+        }
     }
-
-    [HarmonyPrefix]
-    private static bool Prefix(TowerInventory __instance, TowerModel def) => __instance.towerMaxes.ContainsKey(def.baseId);
+    [HarmonyPatch(typeof(TowerInventory),"DestroyedTower")]
+    public class TowerInventoryDestroyedTower_Patch{
+        [HarmonyPrefix]
+        public static bool Prefix(ref TowerInventory __instance,ref TowerModel def){
+            if(!__instance.towerCounts.ContainsKey(def.baseId)){
+                __instance.towerCounts.Add(def.baseId,0);
+            }
+		    __instance.towerCounts[def.baseId]=__instance.towerCounts[def.baseId]-1;
+		    return false;
+        }
+    }
 }
