d51fa48d85bfa76c7792e36335f242e96254aa73
doombubbles <obsidiantoolzg@gmail.com>
2025-08-27 16:23:55 -0700

    Obsolete old AddBehaviorToBloonModelExt

--- BloonsTD6 Mod Helper/Extensions/BehaviorExtensions/AddBehaviorToBloonModelExt.cs
+++ BloonsTD6 Mod Helper/Extensions/BehaviorExtensions/AddBehaviorToBloonModelExt.cs
@@ -1,56 +1,70 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using BTD_Mod_Helper.Api.Helpers;
 using Il2CppAssets.Scripts.Models;
 using Il2CppAssets.Scripts.Models.Bloons;
 using Il2CppAssets.Scripts.Models.Towers.Projectiles.Behaviors;
+
 namespace BTD_Mod_Helper.Extensions;
 
 /// <summary>
 /// Extensions for AddBehaviorToBloonModels
 /// </summary>
+[Obsolete("Use AddBehaviorToBloonModelBehaviorExt")]
 public static class AddBehaviorToBloonModelExt
 {
     /// <inheritdoc cref="ModelBehaviorExt.HasBehavior{T}(Il2CppAssets.Scripts.Models.Model)" />
-    public static bool HasBehavior<T>(this AddBehaviorToBloonModel model) where T : Model =>
+    [Obsolete("Use AddBehaviorToBloonModelBehaviorExt")]
+    public static bool HasBehavior<T>(AddBehaviorToBloonModel model) where T : Model =>
         ModelBehaviorExt.HasBehavior<T>(model);
 
     /// <inheritdoc cref="ModelBehaviorExt.GetBehavior{T}(Il2CppAssets.Scripts.Models.Model)" />
-    public static T GetBehavior<T>(this AddBehaviorToBloonModel model) where T : Model =>
+    [Obsolete("Use AddBehaviorToBloonModelBehaviorExt")]
+    public static T GetBehavior<T>(AddBehaviorToBloonModel model) where T : Model =>
         ModelBehaviorExt.GetBehavior<T>(model);
 
     /// <inheritdoc cref="ModelBehaviorExt.GetBehavior{T}(Il2CppAssets.Scripts.Models.Model,int)" />
-    public static T GetBehavior<T>(this AddBehaviorToBloonModel model, int index) where T : Model =>
+    [Obsolete("Use AddBehaviorToBloonModelBehaviorExt")]
+    public static T GetBehavior<T>(AddBehaviorToBloonModel model, int index) where T : Model =>
         ModelBehaviorExt.GetBehavior<T>(model, index);
 
     /// <inheritdoc cref="ModelBehaviorExt.GetBehavior{T}(Il2CppAssets.Scripts.Models.Model,string)" />
-    public static T GetBehavior<T>(this AddBehaviorToBloonModel model, string nameContains) where T : Model =>
+    [Obsolete("Use AddBehaviorToBloonModelBehaviorExt")]
+    public static T GetBehavior<T>(AddBehaviorToBloonModel model, string nameContains) where T : Model =>
         ModelBehaviorExt.GetBehavior<T>(model, nameContains);
 
     /// <inheritdoc cref="ModelBehaviorExt.GetBehaviors{T}" />
-    public static List<T> GetBehaviors<T>(this AddBehaviorToBloonModel model) where T : Model =>
+    [Obsolete("Use AddBehaviorToBloonModelBehaviorExt")]
+    public static List<T> GetBehaviors<T>(AddBehaviorToBloonModel model) where T : Model =>
         ModelBehaviorExt.GetBehaviors<T>(model).ToList();
 
     /// <inheritdoc cref="ModelBehaviorExt.AddBehavior(Il2CppAssets.Scripts.Models.Model,Il2CppAssets.Scripts.Models.Model)" />
-    public static void AddBehavior<T>(this AddBehaviorToBloonModel model, T behavior) where T : BloonBehaviorModel =>
+    [Obsolete("Use AddBehaviorToBloonModelBehaviorExt")]
+    public static void AddBehavior<T>(AddBehaviorToBloonModel model, T behavior) where T : BloonBehaviorModel =>
         ModelBehaviorExt.AddBehavior(model, behavior);
 
     /// <inheritdoc cref="ModelBehaviorExt.RemoveBehavior{T}(Il2CppAssets.Scripts.Models.Model)" />
-    public static void RemoveBehavior<T>(this AddBehaviorToBloonModel model) where T : Model =>
+    [Obsolete("Use AddBehaviorToBloonModelBehaviorExt")]
+    public static void RemoveBehavior<T>(AddBehaviorToBloonModel model) where T : Model =>
         ModelBehaviorExt.RemoveBehavior<T>(model);
 
     /// <inheritdoc cref="ModelBehaviorExt.RemoveBehavior" />
-    public static void RemoveBehavior<T>(this AddBehaviorToBloonModel model, T behavior) where T : Model =>
+    [Obsolete("Use AddBehaviorToBloonModelBehaviorExt")]
+    public static void RemoveBehavior<T>(AddBehaviorToBloonModel model, T behavior) where T : Model =>
         ModelBehaviorExt.RemoveBehavior(model, behavior);
 
     /// <inheritdoc cref="ModelBehaviorExt.RemoveBehaviors{T}" />
-    public static void RemoveBehaviors<T>(this AddBehaviorToBloonModel model) where T : Model =>
+    [Obsolete("Use AddBehaviorToBloonModelBehaviorExt")]
+    public static void RemoveBehaviors<T>(AddBehaviorToBloonModel model) where T : Model =>
         ModelBehaviorExt.RemoveBehaviors<T>(model);
 
     /// <inheritdoc cref="ModelBehaviorExt.RemoveBehaviors{T}" />
-    public static void RemoveBehaviors(this AddBehaviorToBloonModel model) => ModelBehaviorExt.RemoveBehaviors(model);
+    [Obsolete("Use AddBehaviorToBloonModelBehaviorExt")]
+    public static void RemoveBehaviors(AddBehaviorToBloonModel model) => ModelBehaviorExt.RemoveBehaviors(model);
 
     /// <inheritdoc cref="ModelBehaviorExt.AddBehavior(Il2CppAssets.Scripts.Models.Model,BTD_Mod_Helper.Api.Helpers.ModelHelper)"/>
-    public static void AddBehavior(this AddBehaviorToBloonModel model, ModelHelper behavior) =>
+    [Obsolete("Use AddBehaviorToBloonModelBehaviorExt")]
+    public static void AddBehavior(AddBehaviorToBloonModel model, ModelHelper behavior) =>
         ModelBehaviorExt.AddBehavior(model, behavior);
 }
